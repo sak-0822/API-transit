4e14ea854097ccc1e6e47f54126650692295c593
sak <u695902k@gmail.com>
2022-07-29 23:58:46 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+$(function(){
+$('#Get').click(
+    function(){
+        $.ajax({
+            url:"", //add URL.
+            type:"POST",
+            dataType:'json',
+            contentType:'application/json',
+        })
+    }
+)
+});
